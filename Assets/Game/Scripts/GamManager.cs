c7f8a6dfa2f12e80980ea14487a0e446bce8c4b3
Ghostguitar21 <andresdavid1243@gmail.com>
2026-03-27 19:41:04 -0500

    ff

--- Assets/Game/Scripts/GamManager.cs
+++ Assets/Game/Scripts/GamManager.cs
@@ -1,9 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using System.IO;
 
 public class GamManager : MonoBehaviour
 {
 
+    public static GamManager Instance;
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
     void Start()
     {
         
