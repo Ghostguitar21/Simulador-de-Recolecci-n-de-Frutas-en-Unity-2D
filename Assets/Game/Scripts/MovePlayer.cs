9f45aa5e8e39b818229e967b6d2ef46904240ec6
migue <pasosmiguel822018@gmail.com>
2026-04-08 00:57:09 -0500

    Prefab de personaje y suelo
    En la escena del Nivel 1 se agregaron el suelo y el personaje del jugador.
    El player cuenta con movimiento en el eje X y salto en el eje Y, con animaciones específicas para cada estado: idle (quieto), salto y movimiento horizontal.

---
+++ Assets/Game/Scripts/MovePlayer.cs
@@ -0,0 +1,95 @@
+/*using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * speed, rb.linearVelocity.y);
+    }
+}*/
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 7f;
+    [SerializeField] private LayerMask groundLayer;
+
+    public Animator animator;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    //salto
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Movimiento
+        rb.linearVelocity = new Vector2(moveInput.x * speed, rb.linearVelocity.y);
+
+        // Raycast para detectar suelo
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.2f, groundLayer);
+        isGrounded = hit.collider != null;
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * 1.2f);
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("movement", Mathf.Abs(moveInput.x));
+
+        if (moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
